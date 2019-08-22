69499e987623138c673182cc40ddaff94807951f
Nayana Limaye <nalimaye@yahoo.com>
2019-08-22 16:58:02 -0400

    Student and Test models done

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,35 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
+
+Student.beforeCreate(function(instance) {
+  instance.firstName =
+    instance.firstName[0].toUpperCase() + instance.firstName.slice(1);
+  instance.lastName =
+    instance.lastName[0].toUpperCase() + instance.lastName.slice(1);
+});
 
 module.exports = Student;
